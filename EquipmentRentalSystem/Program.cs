678a0af901a6b0e68ff90bcc309ba255a7f91c32
Roman Denysiuk <r.v.denysiuk@gmail.com>
2026-03-17 14:51:04 +0100

    basic CLI with

--- EquipmentRentalSystem/Program.cs
+++ EquipmentRentalSystem/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("---- Equipment Rental System ----");
+            Console.WriteLine("1. Add new user to the system");
+            Console.WriteLine("2. Add new Equipment item of selected type");
+            Console.WriteLine("3. Display equipment list with current status");
+            Console.WriteLine("4. Display available for rental equipment");
+            Console.WriteLine("5. Rent Equipment");
+            Console.WriteLine("6. Return equipment(possible penalty)");
+            Console.WriteLine("7. Mark equpment as anavalible (for example damage)");
+            Console.WriteLine("8. Display active rental for a selected user");
+            Console.WriteLine("9. Display list of overdue rentals");
+            Console.WriteLine("10. Generate Report of rental Service state");  
+            Console.WriteLine("11. Exit");
+            Console.WriteLine("---------------------------------");
+            
+            Console.WriteLine("Choose an option:");
+            var input =  Console.ReadLine();
+        } 
+    }
 
-Console.WriteLine("Hello, World!");
+}
