db98dcc85b7fff7773935a3d95e195eec5286eba
Roman Denysiuk <r.v.denysiuk@gmail.com>
2026-03-20 21:30:48 +0100

    implemented UserService

---
+++ EquipmentRentalSystem/service/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using EquipmentRentalSystem.models.users;
+
+namespace EquipmentRentalSystem.service;
+
+public interface IUserService
+{
+    User addUser(User user);
+    User getUser(long userId);
+    IEnumerable<User> getUsers();
+}
---
+++ EquipmentRentalSystem/service/UserServiceImpl.cs
@@ -0,0 +1,41 @@
+﻿using EquipmentRentalSystem.models.users;
+using EquipmentRentalSystem.repository;
+
+namespace EquipmentRentalSystem.service;
+
+public class UserServiceImpl : IUserService
+{
+    IUserRepository _userRepository;
+    
+    public UserServiceImpl(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    public User addUser(User user)
+    {
+        if (user == null)
+        {
+            Console.WriteLine("User is null");
+            return null;
+        }
+
+        if (_userRepository.getUser(user.id) != null)
+        {
+            Console.WriteLine("User already exists with given ID: " + user.id);
+            return null;
+        }
+        
+        return _userRepository.addUser(user);
+    }
+
+    public User getUser(long userId)
+    {
+        return _userRepository.getUser(userId);
+    }
+
+    public IEnumerable<User> getUsers()
+    {
+        return _userRepository.getUsers();
+    }
+}
