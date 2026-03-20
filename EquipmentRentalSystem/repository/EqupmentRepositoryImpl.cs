3aa90ec53158ccf7137e51841e32c00e7a1f1f02
Roman Denysiuk <r.v.denysiuk@gmail.com>
2026-03-20 21:47:30 +0100

    implemented equipment repository

---
+++ EquipmentRentalSystem/repository/EqupmentRepositoryImpl.cs
@@ -0,0 +1,29 @@
+﻿using EquipmentRentalSystem.models.equipment;
+
+namespace EquipmentRentalSystem.repository;
+
+public class EqupmentRepositoryImpl : IEquipmentRepository
+{
+    private Dictionary<long, Equipment> equipments = new Dictionary<long, Equipment>();
+    
+    public Equipment addEquipment(Equipment equipment)
+    {
+        equipments.Add(equipment.id, equipment);
+        return equipment;
+    }
+
+    public IEnumerable<Equipment> getEquipments()
+    {
+        return equipments.Values;
+    }
+
+    public Equipment getEquipmentById(long equipmentId)
+    {
+        if (equipments.TryGetValue(equipmentId, out var equipment))
+        {
+            return equipment;
+        }
+        Console.WriteLine("User not found");
+        return null;
+    }
+}
---
+++ EquipmentRentalSystem/repository/IEquipmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using EquipmentRentalSystem.models.equipment;
+
+namespace EquipmentRentalSystem.repository;
+
+public interface IEquipmentRepository
+{
+    Equipment addEquipment(Equipment equipment);
+    IEnumerable<Equipment> getEquipments();
+    Equipment getEquipmentById(long equipmentId);
+}
