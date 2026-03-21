c0b7cc7d1932a21e4150c648828e477e1e5297aa
Roman Denysiuk <r.v.denysiuk@gmail.com>
2026-03-21 12:27:17 +0100

    implemented equipment service

---
+++ EquipmentRentalSystem/service/EquipmentServiceImpl.cs
@@ -0,0 +1,43 @@
+﻿using EquipmentRentalSystem.models.equipment;
+using EquipmentRentalSystem.repository;
+
+namespace EquipmentRentalSystem.service;
+
+public class EquipmentServiceImpl : IEquipmentService
+{
+    
+    private readonly IEquipmentRepository equipmentRepository;
+
+    public EquipmentServiceImpl(IEquipmentRepository equipmentRepository)
+    {
+        this.equipmentRepository = equipmentRepository;
+    }
+
+
+    public Equipment addEquipment(Equipment equipment)
+    {
+        if (equipment == null)
+        {
+            Console.WriteLine("User is null");
+            return null;
+        }
+
+        if (equipmentRepository.getEquipmentById(equipment.id) != null)
+        {
+            Console.WriteLine("Equipment already exists with given ID: " + equipment.id);
+            return null;
+        }
+        
+        return equipmentRepository.addEquipment(equipment);
+    }
+
+    public IEnumerable<Equipment> getEquipments()
+    {
+        return equipmentRepository.getEquipments();
+    }
+
+    public Equipment getEquipmentsById(long equipmentId)
+    {
+        return equipmentRepository.getEquipmentById(equipmentId);
+    }
+}
---
+++ EquipmentRentalSystem/service/IEquipmentService.cs
@@ -0,0 +1,11 @@
+﻿using EquipmentRentalSystem.models.equipment;
+using EquipmentRentalSystem.models.users;
+
+namespace EquipmentRentalSystem.repository;
+
+public interface IEquipmentService
+{
+    Equipment addEquipment(Equipment equipment);
+    IEnumerable<Equipment> getEquipments();
+    Equipment getEquipmentsById(long equipmentId);
+}
