2fa17d9fad083b291eb256f78983a8f2caa7011f
Roman Denysiuk <r.v.denysiuk@gmail.com>
2026-03-21 14:26:46 +0100

    added rental policy and rental service

---
+++ EquipmentRentalSystem/service/IRentPolicyService.cs
@@ -0,0 +1,10 @@
+﻿using EquipmentRentalSystem.models;
+using EquipmentRentalSystem.models.users;
+
+namespace EquipmentRentalSystem.service;
+
+public interface IRentPolicyService
+{ 
+    bool CanUserRent(User user, int activeRents);
+    decimal CalculatePenalty(Rent rent);
+}
---
+++ EquipmentRentalSystem/service/IRentService.cs
@@ -0,0 +1,12 @@
+﻿using EquipmentRentalSystem.models;
+
+namespace EquipmentRentalSystem.service;
+
+public interface IRentService
+{
+    Rent RentEquipment(Guid userId, Guid equipmentId, int days);
+    decimal ReturnEquipment(Guid rentalId);
+    IEnumerable<Rent> GetActiveRentalsByUser(Guid userId);
+    IEnumerable<Rent> GetOverdueRentals();
+    IEnumerable<Rent> GetAllRentals();
+}
